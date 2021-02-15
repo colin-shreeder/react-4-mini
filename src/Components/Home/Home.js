d45d9a196fc513b17674f6139e0d0a9dfca8d239
Colin Reeder <colin.c.reeder@gmail.com>
2021-02-15 14:00:03 -0700

    final

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,8 +5,8 @@ class Home extends Component {
   render() {
     return (
       <div className="Home">
-        <h1>Welcome to the DevMountain tech blog!</h1>
-        <h2>Head over to the topic browser to see all the latest posts!</h2>
+        <h1>Welcome to the Utah Backcountry Blog!</h1>
+        <h2>Browse topics to see our most recent posts!</h2>
       </div>
     );
   }
