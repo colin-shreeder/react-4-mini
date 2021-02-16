9c70ad6f3f9d21ed011b65fee54053adbd3d5c1c
Colin Reeder <colin.c.reeder@gmail.com>
2021-02-15 21:23:23 -0700

    server path

--- server/index.js
+++ server/index.js
@@ -3,8 +3,17 @@ const express = require("express");
 const massive = require('massive');
 const app = express();
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
+const path = require('path')
+
 
 app.use(express.json());
+
+app.use(express.static(__dirname + '/../build'))
+
+app.get('*', (req,res)=>{
+  res.sendFile(path.join(__dirname, '../build/index.html'))
+})
+
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server listening on port ${SERVER_PORT}`);
